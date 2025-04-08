c13dc0a8ba13a8b12d36740e8175152e0d39ae87
myungji <audwl44@naver.com>
2025-04-08 09:48:04 +0900

    summary

---
+++ src/components/Summary.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue';
+</script>
+
+<template>
+  <div class="summary">
+    <h2>월별 요약</h2>
+    <div class="month-summary">
+      <p>총 수입</p>
+      <p>총 지출</p>
+    </div>
+
+    <h2>카테고리별 지출 현황</h2>
+    <div class="category-summary"></div>
+
+    <h2>카테고리별 지출 내역</h2>
+    <div class="category-detail"></div>
+  </div>
+</template>
+
+<style scoped></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+// index.js
+import { createRouter, createWebHistory } from 'vue-router';
+import Summary from '@/components/Summary.vue'; // Summary 컴포넌트 import
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,17 +8,14 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView,
+      component: Summary, // Summary 컴포넌트 사용
     },
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
     },
   ],
-})
+});
 
-export default router
+export default router;
