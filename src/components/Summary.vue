8ee00f29b575e4d01f7d003a4c5a21c51467734c
myungji <audwl44@naver.com>
2025-04-08 10:34:37 +0900

    Summary.vue 수정 및 내용 추가

--- src/components/Summary.vue
+++ src/components/Summary.vue
@@ -4,18 +4,67 @@ import { ref } from 'vue';
 
 <template>
   <div class="summary">
-    <h2>월별 요약</h2>
+    <h2 class="title">월별 요약</h2>
     <div class="month-summary">
-      <p>총 수입</p>
-      <p>총 지출</p>
+      <p class="income">총 수입</p>
+      <p class="expense">총 지출</p>
     </div>
 
-    <h2>카테고리별 지출 현황</h2>
+    <h2 class="summary-title">카테고리별 지출 현황</h2>
     <div class="category-summary"></div>
 
-    <h2>카테고리별 지출 내역</h2>
+    <h2 class="detail-title">카테고리별 지출 내역</h2>
     <div class="category-detail"></div>
   </div>
 </template>
 
-<style scoped></style>
+<!-- 스타일 생성 -->
+<style scoped>
+.summary {
+  text-align: center;
+}
+
+.title {
+  margin-bottom: 20px;
+  position: fixed;
+  top: 0;
+  text-align: center;
+  left: 37%;
+}
+
+/* 월별 요약 */
+.month-summary {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 30px;
+}
+
+/* 총 수입과 지출 */
+.income,
+.expense {
+  border: 1px solid;
+  padding: 10px 20px;
+  border-radius: 5px;
+  width: 150px;
+}
+.summary-title,
+.detail-title {
+  font-size: 14px;
+}
+
+/* 카테고리별 지출현황 */
+.category-summary {
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  margin: 20px auto;
+  background-color: yellow;
+}
+/* 카테고리별 지출내역 */
+.category-detail {
+  border: 1px solid;
+  padding: 20px;
+  margin-top: 30px;
+  min-height: 150px;
+}
+</style>
