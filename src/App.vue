5963db5bdbda6c5b499b3c9e23db0e209c038110
hannie <>
2025-04-10 09:20:43 +0900

    달력추가, 라우터 추가

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <script setup>
-
 import LoginPage from '@/views/LoginPage.vue';
 import Summary from '@/views/Summary.vue';
 import { RouterLink, RouterView } from 'vue-router';
@@ -10,16 +9,14 @@ import { RouterLink, RouterView } from 'vue-router';
     <header>
       <h1>나의 가계부</h1>
       <nav>
-        <RouterLink to="/record">기록</RouterLink> |
-        <RouterLink to="/transactions">거래 조회</RouterLink>|
-        <RouterLink to="/login">로그인</RouterLink> |
-        <RouterLink to="/join">회원가입</RouterLink>|
+        <RouterLink to="/">홈</RouterLink> | <RouterLink to="/record">기록</RouterLink> |
+        <RouterLink to="/summary">요약</RouterLink>| <RouterLink to="/transactions">거래 조회</RouterLink> |
+        <RouterLink to="/Management">재정관리</RouterLink>
       </nav>
     </header>
 
     <main>
       <RouterView />
-      <!-- <Summary /> -->
     </main>
 
     <footer>
